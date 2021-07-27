1376d03a13c211fdf7fc3529473e9a1754397359
LeeHsss <lii0730@icloud.com>
2021-07-27 21:50:34 +0900

    Initial Commit

---
+++ SwiftUI_stack_master/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_stack_master
+//
+//  Created by LeeHsss on 2021/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_stack_master/SwiftUI_stack_masterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_stack_masterApp.swift
+//  SwiftUI_stack_master
+//
+//  Created by LeeHsss on 2021/07/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_stack_masterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
